db412cf726607402f88bc24d0286a46a5de94302
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-20 21:37:29 +0530

    Created conversation UI

---
+++ components/heading.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+interface HeadingProps {
+  title: string;
+  description: string;
+  icon: any;
+  iconColor?: string;
+  bgColor?: string;
+}
+
+const Heading: React.FC<HeadingProps> = ({
+  title,
+  description,
+  icon: Icon,
+  iconColor,
+  bgColor,
+}) => {
+  return (
+    <div className='px-4 lg:px-8 flex items-center gap-x-3 mb-8'>
+      <div className={cn("p-2 rounded-md w-fit", bgColor)}>
+        <Icon className={cn("w-10 h-10", iconColor)} />
+      </div>
+      <div>
+        <h1 className='text-3xl font-bold'>{title}</h1>
+        <p className='text-sm text-muted-foreground'>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Heading;
