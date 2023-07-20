26c1bd653d47ff567edfba6b860b5ee52b955867
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-07-20 13:31:30 +0530

    Created app pages and folder structure

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Menu } from "lucide-react";
+import { Sidebar } from "@/components/sidebar";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+  return (
+    <div>
+      <Sheet>
+        <SheetTrigger>
+          <Button variant={"ghost"} size={"icon"} className='md:hidden'>
+            <Menu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side={"left"} className='p-0'>
+          <Sidebar />
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+
+export default MobileSidebar;
